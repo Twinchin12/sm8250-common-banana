955ced6761a14b6029ce5ac6f6d47f04ba38c0d8
Garret Kelly <gdk@google.com>
2019-06-24 11:57:44 -0400

    sm8250-common: Set BOOT_SECURITY_PATCH during build
    This property is now used during boot to provide the TEE-backed
    keymaster with the correct boot SPL.  Set it to the platform SPL until
    it diverges as part of an update.
    
    Bug: 119549677
    Test: verified boot.img has property using avbtool
    Change-Id: Ice8e7747059492fc8729128e99119d8678fe5562

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -175,6 +175,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SEPolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
